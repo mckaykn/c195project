bde70b72263b47b6a6454b17ce79cd7c6552f47b
mckaykn <mckayknielson@gmail.com>
2023-04-10 00:26:44 -0600

    Wrote some javadoc. Fixed editAppointment and addAppointment so they catch all overlapping appointments, not just for the same customer.

--- src/main/java/nielson/c195projectmkn/Models/Month.java
+++ src/main/java/nielson/c195projectmkn/Models/Month.java
@@ -1,23 +1,54 @@
 package nielson.c195projectmkn.Models;
 
+/**
+
+ The Month class represents a calendar month.
+ */
 public class Month {
+
+    /**
+
+     The ID of the month.
+     */
     public int id;
+    /**
+
+     The name of the month.
+     */
     public String name;
+    /**
+
+     Constructs a Month object with the given ID and name.
+     @param id the ID of the month.
+     @param name the name of the month.
+     */
     public Month(int id, String name) {
         this.id = id;
         this.name = name;
     }
+    /**
 
-    @Override
-    public String toString() {
+     Returns the name of the month.
+     @return the name of the month.
+     */
+    public String getName() {
         return name;
     }
+    /**
 
+     Returns the ID of the month.
+     @return the ID of the month.
+     */
     public int getId() {
         return id;
     }
+    /**
 
-    public String getName() {
+     Returns the name of the month as a string.
+     @return the name of the month as a string.
+     */
+    @Override
+    public String toString() {
         return name;
     }
 }
